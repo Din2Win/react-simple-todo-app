40e49fc1ff66aa565d9e120f7cdea0e14f48e7ce
Din2Win <dinkarnitskiy@gmail.com>
2019-01-24 15:57:34 -0500

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import uuidv4 from 'uuid/v4';
+
+import TaskView from './components/view/TaskView';
+import Nav from './components/container/Nav';
+import TaskInput from './components/container/TaskInput';
 
 class App extends Component {
+
+  state = {
+    todo: [
+      {
+        task: "Buy Milk",
+        completed: false,
+        id: uuidv4()
+      }
+    ]
+  }
+
+  handleSubmit = (task) => {
+    let newItem = {
+      task: task,
+      completed: false, 
+      id: uuidv4()
+    }
+    let currentItem = Object.assign([], this.state.todo);
+    currentItem.push(newItem);
+
+    this.setState({
+      todo: currentItem
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Nav />
+        <TaskInput 
+          handleSubmit={this.handleSubmit} 
+        />
+        {
+          this.state.todo.map((element) => {
+            return (
+              <TaskView 
+                key={element.id}
+                task={element.task}
+                />
+            )
+          })
+        }
       </div>
     );
   }
---
+++ src/components/view/TaskView.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+
+const TaskView = (props) => {
+    return (
+        <div>
+            Something I need to do : {props.task}
+        </div>
+    )
+}
+
+function addTask(event) {
+
+
+
+    
+}
+
+
+export default TaskView;
+
